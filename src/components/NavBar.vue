5c3d161a98c5a0dcde4e1b4cc3b58e2e411f8fb4
JoladeFash <thejoladefashakin@gmail.com>
2019-11-22 16:31:22 +0100

    File

---
+++ src/components/NavBar.vue
@@ -0,0 +1,53 @@
+<template>
+  <!--<div id="nav" class="nav">
+    <router-link to="/" class="brand">Real World Events</router-link>
+    <nav>
+      <router-link :to="{ name: 'event-list' }">List</router-link> |
+      <router-link :to="{ name: 'event-create' }">Create</router-link>
+    </nav>
+  </div>-->
+ <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+    <div class="container">
+      <router-link class="navbar-brand js-scroll-trigger" to="/">Find Courses</router-link>
+      <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        Menu
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item mx-0 mx-lg-1">
+               <router-link class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" to="/">Home</router-link>
+            
+          </li>
+          <li class="nav-item mx-0 mx-lg-1">
+            <a style="cursor: pointer" v-if="isAuth"  class="nav-link" @click="logout">Logout</a>
+            
+            <router-link v-else class="nav-link" to="/signup">Login</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+
+         
+</template>
+
+<script>
+  import {mapGetters} from 'vuex'
+  
+    export default {
+        computed:{
+          isAuth(){
+            return this.$store.getters.isAuth;
+          }
+        },
+      methods:{
+        logout(){
+          console.log('lodgout');
+          this.$store.commit('UNSET_USER');
+          this.$router.push('/');
+        }
+      }
+    }
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,48 @@
+<template>
+ <header class="masthead bg-primary text-white text-center">
+    <div class="container d-flex align-items-center flex-column">
+
+      <!-- Masthead Avatar Image -->
+      <img class="masthead-avatar mb-5" src="img/avataaars.svg" alt="">
+
+      <!-- Masthead Heading -->
+      <h1 class="masthead-heading text-uppercase mb-0">Welcome!</h1>
+
+      <!-- Icon Divider -->
+      <div class="divider-custom divider-light">
+        <div class="divider-custom-line"></div>
+        <div class="divider-custom-icon">
+          <i class="fas fa-star"></i>
+        </div>
+        <div class="divider-custom-line"></div>
+      </div>
+      <router-link class="btn btn-primary btn-lg" to="/signup" role="button">Sign up</router-link>
+
+      <!-- Masthead Subheading -->
+      <p class="masthead-subheading font-weight-light mb-0">Find After School Activities for Students</p>
+
+    </div>
+  </header>
+
+
+
+
+
+  <!-- <div class="jumbotron">
+    <div class="container">
+      <h1 class="display-4">Welcome!</h1>
+      <p class="lead">You can choose Extra Curricular Activities and Courses</p>
+      <hr class="my-4">
+      <p>Trying to stay at the top? Register with us and get access to a wide range os extra curricluar activities and courses.</p>
+      <router-link class="btn btn-primary btn-lg" to="/signup" role="button">Sign up</router-link>
+    </div>
+  </div> -->
+</template>
+<script>
+export default {
+  filterResutl(){
+
+  }
+}
+</script>
+
